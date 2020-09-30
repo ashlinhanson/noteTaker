34a93e87f64cdb6a94104b639dac8b0fd277bf96
ashlinhanson <ashlinhanson@gmail.com>
2020-09-30 13:43:42 -0500

    adding notes for the heroku changes needed to be made

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-var PORT = 8080;
+var PORT = 8080;//dont forget to change this for heroku. process.env.PORT || 9080
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
