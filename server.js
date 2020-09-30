64068c7cb22bccd498043fdb75dd672511ed742c
ashlinhanson <ashlinhanson@gmail.com>
2020-09-30 13:40:09 -0500

    added 'get' code

--- server.js
+++ server.js
@@ -7,3 +7,24 @@ var PORT = 8080;
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
+
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.get("/api/notes", function(req, res){
+    return res.join(notes);
+});
+
+app.get("/api/notes/:note", function(req, res){
+    var chosenNote = req.params.notes;
+    console.log(chosenNote);
+
+    for(var i = 0; i < notes.length; i++) {
+        if (chosenNote === notes[i].routeName) {
+        return res.json(notes[i]);
+        }
+    }
+    return res.join(false);
+});
