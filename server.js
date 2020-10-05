c2e0fbd7990337b8bc75d8c8d90668f2af941350
ashlinhanson <ashlinhanson@gmail.com>
2020-10-05 12:39:26 -0500

    added uuid, post function

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid");
+
+const notesArr = require("./db/db.json");
 
 // preps Express to be used
 const app = express();
@@ -9,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 // prepping the express app to handle the data that will be input
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
+app.use(express.static("./public"));
 
  //--------HTML Routes---------
 // gets the notes.html file
@@ -19,6 +23,18 @@ app.get("/notes", function(req, res){
 app.get("*", function(req, res){
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
+
+app.post(".api/notes", function(req, res){
+    const newNote = {
+        id : uuid.v4(),
+        title : req.body.title,
+        text : req.body.text
+    }
+    notesArr.push(newNotes);
+    return notesArr;
+});
+
+app.delete("")
 
 // Activates the server to begin listening
 app.listen(PORT, () => {
